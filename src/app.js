8731b49316c25bdad7984ce46fd7e2aaefdd5139
barbosa92 <alexrp_92@hotmail.com>
2022-04-21 16:04:29 +0000

    FiniFinished proyect with timer, button and size inputs

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
+
+// Genera una carta de forma aleatoria
 function randomGenerator() {
   let suits = ["♦", "♥", "♠", "♣"];
   let numbers = [
@@ -53,57 +55,55 @@ function randomGenerator() {
   numberValue.innerHTML = number;
 }
 
+// Lanza la función randomGenerator al hacer click en el botón
 const generator = document.querySelector("button");
 generator.addEventListener("click", randomGenerator);
 
-window.onload = function() {
-  let suits = ["♦", "♥", "♠", "♣"];
-  let numbers = [
-    "A",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K"
-  ];
+window.onload = randomGenerator();
 
-  let suit = suits[Math.floor(Math.random() * suits.length)];
-  let number = numbers[Math.floor(Math.random() * numbers.length)];
+// Recoge el valor de ancho registrado en el input lo lo añade como CSS inline al HTML
+let inputAncho = document.querySelector(".ancho");
+inputAncho.addEventListener("keypress", event => {
+  // Guarda el valor al pulsar enter
+  if (event.keyCode == 13) {
+    // Formatea "valor" para que sea "number"
+    let valor = Number(document.querySelector(".ancho").value);
+    document.querySelector(".ancho").value = "";
 
-  let icon = document.querySelectorAll("i");
-  for (let index = 0; index < icon.length; index++) {
-    icon[index].innerHTML = suit;
-    const attribute = document.createAttribute("class");
-    if (suit == "♦" || suit == "♥") {
-      attribute.value = "red";
+    // Comprueba que el valor introducido sea un número mayor a 0
+    if (typeof valor == "number" && valor > 0) {
+      // console.log("Valor valido");
+      // console.log(valor);
+
+      let card = document.querySelector(".card");
+      card.style.width = `${valor}px`;
     } else {
-      attribute.value = "black";
+      console.log("Ingresa un valor númerico mayo a 0");
     }
-    icon[index].setAttributeNode(attribute);
   }
+});
 
-  // Crea un atributo "class"
-  const attribute = document.createAttribute("class");
+// Recoge el valor de alto registrado en el input lo lo añade como CSS inline al HTML
+let inputAlto = document.querySelector(".alto");
+inputAlto.addEventListener("keypress", event => {
+  // Guarda el valor al pulsar enter
+  if (event.keyCode == 13) {
+    // Formatea "valor" para que sea "number"
+    let valor = Number(document.querySelector(".alto").value);
+    document.querySelector(".alto").value = "";
 
-  // Define el valor de "class" según el valor de "suit"
-  if (suit == "♦" || suit == "♥") {
-    attribute.value = "red";
-  } else {
-    attribute.value = "black";
+    // Comprueba que el valor introducido sea un número mayor a 0
+    if (typeof valor == "number" && valor > 0) {
+      // console.log("Valor valido");
+      // console.log(valor);
+
+      let card = document.querySelector(".card");
+      card.style.height = `${valor}px`;
+    } else {
+      console.log("Ingresa un valor númerico mayo a 0");
+    }
   }
+});
 
-  // Añade el atributo a l <h1>
-  const h1 = document.querySelector("h1");
-  h1.setAttributeNode(attribute);
-
-  // Añade el valor de la carta al <h1>
-  let numberValue = document.querySelector("h1");
-  numberValue.innerHTML = number;
-};
+// Llama a la función randomGenerator cada 5 segundos
+setInterval(randomGenerator, 5000);
